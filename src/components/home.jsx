6376cfbe8164ea2d21ddb73221e35f9cb5671f8f
Cristian Zambrano <criszamcham@gmail.com>
2023-08-29 14:48:32 -0400

    Add new carousel component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,37 +1,30 @@
-import React from 'react';
 import '../styles/Home.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquareCaretLeft, faSquareCaretRight } from '@fortawesome/free-solid-svg-icons';
-import EmblaCarousel from './Carousel';
-import data from '../data/data.json';
-import RoomList from './RoomList';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-const options = {
-  dragFree: true,
-  containScroll: 'trimSnaps',
-};
-const SLIDE_COUNT = data.length / 3;
-const slides = Array.from(Array(SLIDE_COUNT).keys()).map(() => data);
+const Home = () => {
+  const responsive = {
+    superLargeDesktop: {
+    // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
 
-const Home = () => (
-  <>
+  };
+  return (
     <section className="home-container">
-      <div className="flex home-header">
-        <h1 className="title">Our Rooms</h1>
-        <p className="header-text">Pleas select a room</p>
-      </div>
-      <div className="arrows">
-        <div className="arrow arrow-left">
-          <FontAwesomeIcon icon={faSquareCaretLeft} />
-        </div>
-        <div className="arrow arrow-right">
-          <FontAwesomeIcon icon={faSquareCaretRight} />
-        </div>
-      </div>
-      <EmblaCarousel slides={slides} options={options} />
-      <RoomList />
+      <Carousel responsive={responsive}>
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+        <div>Item 5</div>
+        <div>Item 6</div>
+        <div>Item 7</div>
+      </Carousel>
+      ;
     </section>
-  </>
-);
+  );
+};
 
 export default Home;
