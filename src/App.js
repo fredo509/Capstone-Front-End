ab6a7e3343e75fefc89ad57c7cadb44eca4a0da4
Cristian Zambrano <criszamcham@gmail.com>
2023-08-28 21:14:07 -0400

    Add styling to navbar items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import './styles/Navbar.scss';
 import Reservations from './components/Reservations';
 import Navbar from './components/navbar';
 import Home from './components/home';
+import Rooms from './components/RoomList';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reservations" element={<Reservations />} />
+        <Route path="/rooms" element={<Rooms />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import logo from '../assets/hotelLogo.png';
 
 const Navbar = () => (
-  <ul className="navbar">
-    <li>
-      <NavLink to="/">Rooms</NavLink>
-    </li>
-    <li>
-      <NavLink to="/Reservations">My Reservations</NavLink>
-    </li>
-    <li>
-      <NavLink to="/">Rooms</NavLink>
-    </li>
-    <li>
-      <NavLink to="/">Rooms</NavLink>
-    </li>
-    <li>
-      <NavLink to="/">Rooms</NavLink>
-    </li>
-  </ul>
+  <div className="navbar">
+    <img src={logo} alt="logo" className="navLogo" />
+    <ul className="navItems">
+      <li className="navItem active">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li className="navItem">
+        <NavLink to="/reservations">Make a reservation</NavLink>
+      </li>
+      <li className="navItem">
+        <NavLink to="/rooms">My Reservations</NavLink>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Navbar;
