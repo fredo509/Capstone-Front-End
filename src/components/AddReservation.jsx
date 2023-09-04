0388e5dc32568b4d22060272b694bb965a68cd59
Cristian Zambrano <criszamcham@gmail.com>
2023-09-04 14:09:03 -0400

    Modify slicer

--- src/components/AddReservation.jsx
+++ src/components/AddReservation.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBranches } from '../redux/roomsSlice'; // Import the fetchBranches action
+
 import '../styles/Home.scss';
 
-const AddReservation = () => (
-  <>
-    <section className="home-container">
-      <h1 className="home-title">Reserve your Suite</h1>
-      <p className="home-brief">Please select where you want to stay</p>
-      <p className="home-brief">2 link, one to branch A other to branch B</p>
-    </section>
-  </>
-);
+const AddReservation = () => {
+  console.log('Rendering');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBranches());
+  }, [dispatch]);
+
+  const branches = useSelector((state) => state.branches);
+
+  console.log('branches:', branches);
+
+  return (
+    <>
+      <section className="home-container">
+        <h1 className="home-title">Reserve your Suite</h1>
+        <p className="home-brief">Please select where you want to stay</p>
+        <ul>
+          {branches.map((branch) => (
+            <li key={branch.id}>{branch.name}</li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
 
 export default AddReservation;
